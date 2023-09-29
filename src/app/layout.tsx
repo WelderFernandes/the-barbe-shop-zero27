2f685f4353de0668f45cbbba0c7bb0c49cc1cabd
Welder Fernandes <welderx3@gmail.com>
2023-09-29 18:43:26 -0300

    ajustes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <body className={inter.className}>
-        {children}
-      </body>
     </html>
   )
 }
