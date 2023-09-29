db88bba4965570d3b19ef4baee7432f555499acc
Welder Fernandes <welderx3@gmail.com>
2023-09-29 18:12:35 -0300

    ajusted and create configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,27 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#111827',
+          secondary: '#f3f4f6',
+          accent: '#f3e8ff',
+          neutral: '#1f1825',
+          'base-100': '#e9e8ed',
+          info: '#4aa0de',
+          success: '#22c55e',
+          warning: '#f8a116',
+          error: '#dc2626',
+        },
+      },
+    ],
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
 export default config
