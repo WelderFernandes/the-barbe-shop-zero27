feb96fe7b12be5f11fd312826e227716adda9fd1
Welder Fernandes <welderx3@gmail.com>
2023-09-29 18:53:13 -0300

    ajuste

--- src/app/agendamento/layout.tsx
+++ src/app/agendamento/layout.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from 'react'
 interface AgendamentoLayoutProps {
   children: ReactNode
 }
-
 export default function AgendamentoLayout({ children }: AgendamentoLayoutProps) {
   return (
     <section className="m-auto h-auto min-h-screen w-full max-w-[425px] bg-[#F1F1F1]">
