b366995a6ba2919e0ef9e259c92d8151bf25b72e
Welder Fernandes <welderx3@gmail.com>
2023-09-29 18:46:44 -0300

    ajustes

--- src/app/agendamento/layout.tsx
+++ src/app/agendamento/layout.tsx
@@ -1,13 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react'
 
 interface AgendamentoLayoutProps {
-    children: ReactNode
+  children: ReactNode
 }
 
 export default function AgendamentoLayout({ children }: AgendamentoLayoutProps) {
-    return ( 
-        <section className="min-h-screen h-auto m-auto w-full max-w-[425px] bg-[#F1F1F1]">
-            {children}
-        </section>
-    );
+  return (
+    <section className="m-auto h-auto min-h-screen w-full max-w-[425px] bg-[#F1F1F1]">
+      {children}
+    </section>
+  )
 }
--- src/app/components/rodape.tsx
+++ src/app/components/rodape.tsx
@@ -1,14 +1,22 @@
-import { AiOutlineCalendar, AiOutlineHeart, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { AiOutlineCalendar, AiOutlineHeart, AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
 
 export default function Rodape() {
-    return (
-        <>
-            <div className="tabs tabs-boxed flex justify-between items-center fixed bottom-0 max-w-[425px] w-full h-[80px] bg-white shadow-md rounded-t-2xl">
-                <a className="tab text-black text-4xl"><AiOutlineHome/></a> 
-                <a className="tab text-black text-4xl"><AiOutlineHeart/></a> 
-                <a className="tab text-black text-4xl"><AiOutlineCalendar/></a>
-                <a className="tab text-black text-4xl"><AiOutlineUser/></a>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="tabs-boxed tabs fixed bottom-0 flex h-[80px] w-full max-w-[425px] items-center justify-between rounded-t-2xl bg-white shadow-md">
+        <a className="tab text-4xl text-black">
+          <AiOutlineHome />
+        </a>
+        <a className="tab text-4xl text-black">
+          <AiOutlineHeart />
+        </a>
+        <a className="tab text-4xl text-black">
+          <AiOutlineCalendar />
+        </a>
+        <a className="tab text-4xl text-black">
+          <AiOutlineUser />
+        </a>
+      </div>
+    </>
+  )
 }
